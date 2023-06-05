32ae4b1fd3de1e7c393b8c88f80b0017c8c08d33
aldoalfians <aldoboetd908@gmail.com>
2023-06-05 12:51:40 +0700

    update auth using token

--- src/components/Userlist.jsx
+++ src/components/Userlist.jsx
@@ -1,30 +1,43 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Userlist = () => {
   const [users, setUsers] = useState([]);
+  const { userInfo, userToken } = useSelector((state) => state.auth);
 
   useEffect(() => {
     getUsers();
   }, []);
 
   const getUsers = async () => {
-    const response = await axios.get("http://localhost:5000/users");
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        "x-auth-token": userToken,
+      },
+    };
+    const response = await axios.get("http://localhost:5000/users", config);
     setUsers(response.data);
   };
 
   const deleteUser = async (userId) => {
-    await axios.delete(`http://localhost:5000/users/${userId}`);
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        "x-auth-token": userToken,
+      },
+    };
+    await axios.delete(`http://localhost:5000/users/${userId}`, config);
     getUsers();
   };
 
   return (
     <div>
       <h1 className="title">Users</h1>
-      <h2 className="subtitle">List of Users</h2>
       <Link to="/users/add" className="button is-primary mb-2">
-        Add New
+        Tambah User
       </Link>
       <table className="table is-striped is-fullwidth">
         <thead>
