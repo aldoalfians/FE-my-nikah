c8a5116e22e122da777aab8d6f087f4932e2307d
aldoalfians <aldoboetd908@gmail.com>
2023-06-11 14:09:00 +0700

    add any function

---
+++ src/utils/constant.js
@@ -0,0 +1,16 @@
+export const Constant = {
+  BASE_URL: "http://localhost:5000",
+};
+
+export const FORM_LAYOUT = {
+  labelCol: { span: 24, md: 16 },
+  wrapperCol: { span: 24, md: 16 },
+};
+
+export const FORM_VALIDATE_MESSAGE = {
+  required: "${label} wajib diisi!",
+  string: {
+    min: "${label} minimal ${min} karakter!",
+    max: "${label} minimal ${mxn} karakter!",
+  },
+};
